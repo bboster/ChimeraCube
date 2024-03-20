42a22d5fec2936cb96997909867c59ea69fec6cc
Trinity <thutson3876@gmail.com>
2024-03-20 14:46:42 -0500

    Implemented Arm Growth

---
+++ ChimeraCube/Assets/ScriptableObjects/Scripts/ArmSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ArmSO : ScriptableObject
+{
+    public float maxHealth = 100;
+
+    public float maxStamina = 100;
+
+    public float staminaRegen = 5;
+
+    public float maxStretchLength = 5;
+}
--- ChimeraCube/Assets/Scripts/Chimera Abilities/Arm.cs
+++ ChimeraCube/Assets/Scripts/Chimera Abilities/Arm.cs
@@ -2,15 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Author: Trinity
+/// Description: Controls Arm behavior
+/// </summary>
 public class Arm : MonoBehaviour
 {
     [SerializeField]
+    ArmSO armData;
+
+    [Header("Logic Assignments")]
+    [SerializeField]
     ChimeraAbility abil;
 
-    // Update is called once per frame
+    [SerializeField]
+    Transform handTransform;
+
+    [SerializeField]
+    Transform armTransform;
+
+    [SerializeField]
+    Collider handCollider;
+
+    [SerializeField]
+    Collider armCollider;
+
+    [Header("Visual Assignments")]
+    [SerializeField]
+    Renderer handRenderer;
+
+    [SerializeField]
+    Renderer armRenderer;
+
+    float currentHealth = 100;
+
+    float currentStamina = 0;
+
+    private void Start()
+    {
+        currentStamina = 0;
+        currentHealth = armData.maxHealth;
+    }
+
+    // Stamina and Ability Tracking
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))
             abil.Execute();
+    }
+
+    private void FixedUpdate()
+    {
+        Tick(Time.fixedDeltaTime);
+    }
+
+    private void Tick(float deltaTime)
+    {
+        if (!IsFullyStretched())
+            currentStamina += deltaTime * armData.staminaRegen;
+
+        Stretch();
+    }
+
+    public bool IsFullyStretched()
+    {
+        return currentStamina >= armData.maxStamina;
+    }
+
+    // Stretching
+    private void Stretch()
+    {
+        float newScale = (currentStamina / armData.maxStamina) * armData.maxStretchLength;
+
+        Vector3 tempScale = armTransform.localScale;
+        tempScale.z = newScale;
+        armTransform.localScale = tempScale;
+
+        tempScale = handTransform.localPosition;
+        tempScale.z = newScale * 2;
+        handTransform.localPosition = tempScale;
+    }
+
+    // Health and Damaging
+    public void Damage(float dmg)
+    {
+        currentHealth -= dmg;
+        Debug.Log(gameObject + " ouch!");
+
+        if (currentHealth <= 0)
+        {
+            Debug.Log(gameObject + " died!");
+            return;
+        }
     }
 }
